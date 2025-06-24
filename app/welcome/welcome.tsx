fb677bdaca6d93d10217ffd4fe565fefa82d8fbc
Khalid Adan <KhalidAdan@users.noreply.github.com>
2025-06-23 21:07:03 -0400

    Update welcome.tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,12 +6,12 @@ export function Welcome() {
       <h1 className="text-4xl md:text-6xl lg:text-8xl font-mono text-black">
         $1 MILLION{" "}
         <Link
-          to="https://xe.com/currencyconverter/convert/?Amount=1000000&From=CAD&To=USD"
+          to="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQxPoL9EKc_DjFPFYNmAwAm7bo6f_g07Ixuzd2hZIj86vRmU6pZ"
           target="_blank"
           rel="noopener noreferrer"
           className="underline hover:no-underline transition-all"
         >
-          CAD
+          DOLLARS
         </Link>
       </h1>
 
